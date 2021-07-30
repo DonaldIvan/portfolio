b3da308915ca6d6d2cbb0c05fd6f25ad68ef25ac
DonaldIvan <developer.donaldivan@gmail.com>
2021-07-30 08:59:24 +0800

    hero section

---
+++ components/Hero/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from 'styles/GlobalComponents';
+import Button from 'styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero: React.FC = () => (
+  <Section>
+    <LeftSection>
+      <SectionTitle main center>
+        Hi, I&#39;m Donald Ivan
+        <br />
+      </SectionTitle>
+      <SectionText>Text</SectionText>
+      <Button>Learn more</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
